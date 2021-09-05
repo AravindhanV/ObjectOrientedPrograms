7c30d010cc7d2a46b554bc3f4f3ee870b8691db5
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 16:18:42 +0530

    ADDED: created Inventory class with getters and setters

---
+++ src/com/bridgelabz/inventorymanagement/Inventory.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.inventorymanagement;
+
+public class Inventory {
+	private String name;
+	private Double weight;
+	private Double pricePerKG;
+	
+	public Inventory(String name, Double weight, Double pricePerKG) {
+		this.name=name;
+		this.weight=weight;
+		this.pricePerKG=pricePerKG;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Double getPricePerKG() {
+		return pricePerKG;
+	}
+
+	public void setPricePerKG(Double pricePerKG) {
+		this.pricePerKG = pricePerKG;
+	}
+}
