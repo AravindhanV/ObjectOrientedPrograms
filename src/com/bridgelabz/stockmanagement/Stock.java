92491b533300dba4b6b657b0150a9ef7fdefa907
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 16:39:16 +0530

    ADDED: created variables, getters and setters for Stock

--- src/com/bridgelabz/stockmanagement/Stock.java
+++ src/com/bridgelabz/stockmanagement/Stock.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.stockmanagement;
 
 public class Stock {
+	private int noOfShares;
+	private int sharePrice;
+	private String shareName;
 
+	public int getNoOfShares() {
+		return noOfShares;
+	}
+
+	public void setNoOfShares(int noOfShares) {
+		this.noOfShares = noOfShares;
+	}
+
+	public int getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(int sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+
+	public String getShareName() {
+		return shareName;
+	}
+
+	public void setShareName(String shareName) {
+		this.shareName = shareName;
+	}
 }
