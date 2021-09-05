9b42f2453f99d64bad0f0678119ccd84125eb3b7
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 16:25:22 +0530

    ADDED: added code to output inventory values

--- src/com/bridgelabz/inventorymanagement/InventoryMain.java
+++ src/com/bridgelabz/inventorymanagement/InventoryMain.java
@@ -4,6 +4,10 @@ public class InventoryMain {
 	public static void main(String[] args) {
 		Inventory rice=new Inventory("Rice",40.5, 38.8);
 		Inventory wheat=new Inventory("Wheat",23.6, 17.5);
-		Inventory pulses=new Inventory("Rice",10.4, 30.5);
+		Inventory pulses=new Inventory("Pulses",10.4, 30.5);
+		
+		System.out.println("Value of "+rice.getName()+" : "+rice.calculateValue());
+		System.out.println("Value of "+wheat.getName()+" : "+wheat.calculateValue());
+		System.out.println("Value of "+pulses.getName()+" : "+pulses.calculateValue());
 	}
 }
