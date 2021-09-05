5875bcd24bcc1b26683c6eac132d4f60c147a511
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 20:13:56 +0530

    ADDED: Implemented buy and sell methods for StockAccount

--- src/com/bridgelabz/commercialprocessing/StockAccount.java
+++ src/com/bridgelabz/commercialprocessing/StockAccount.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.commercialprocessing;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class StockAccount {
@@ -16,11 +17,38 @@ public class StockAccount {
 		symbol = symbol.toUpperCase();
 		boolean isFound = false;
 		for(CompanyShares share : companyShares) {
-			
+			if(share.getSymbol().equals(symbol)) {
+				isFound=true;
+				int currentAmount = share.getNoOfShares();
+				share.setNoOfShares(currentAmount + amount);
+				break;
+			}
+		}
+		
+		if(!isFound) {
+			companyShares.add(new CompanyShares(symbol, amount, LocalDateTime.now()));
 		}
 	}
 	
 	public void sell(int amount, String symbol) {
+		symbol = symbol.toUpperCase();
+		boolean isFound = false;
+		for(CompanyShares share : companyShares) {
+			if(share.getSymbol().equals(symbol)) {
+				isFound=true;
+				int currentAmount = share.getNoOfShares();
+				if(currentAmount < amount) {
+					System.out.println("Insufficient stock amount");
+					return;
+				}
+				share.setNoOfShares(currentAmount - amount);
+				break;
+			}
+		}
+		
+		if(!isFound) {
+			System.out.println("Symbol doesn't exist");
+		}
 		
 	}
 	
