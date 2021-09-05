d0f431f99576f1683bf9e2629b2c7fc5fe989252
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 16:21:28 +0530

    ADDED: Created a main class for execution

---
+++ src/com/bridgelabz/inventorymanagement/InventoryMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inventorymanagement;
+
+public class InventoryMain {
+	public static void main(String[] args) {
+		Inventory rice=new Inventory("Rice",40.5, 38.8);
+		Inventory wheat=new Inventory("Wheat",23.6, 17.5);
+		Inventory pulses=new Inventory("Rice",10.4, 30.5);
+	}
+}
