3a3732b253527a0f515629bed75090e34457f0b9
Aravindhan V <aravindhan.v@perfios.com>
2021-09-05 19:55:54 +0530

    ADDED: created CompanyShares class

---
+++ src/com/bridgelabz/commercialprocessing/CompanyShares.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.commercialprocessing;
+
+import java.time.LocalDateTime;
+
+public class CompanyShares {
+	private String symbol;
+	private int noOfShares;
+	private LocalDateTime dataTime;
+}
--- src/com/bridgelabz/commercialprocessing/StockAccount.java
+++ src/com/bridgelabz/commercialprocessing/StockAccount.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.commercialprocessing;
 
 public class StockAccount {
+	private CompanyShares[] companyShares;
 	public StockAccount() {
-		
+		companyShares = new CompanyShares[10];		
 	}
 	
 	public double valueOf() {
 		return 0;
 	}
 	
-	public void buy() {
+	public void buy(int amount, String symbol) {
 		
 	}
 	
-	public void sell() {
+	public void sell(int amount, String symbol) {
 		
 	}
 	
