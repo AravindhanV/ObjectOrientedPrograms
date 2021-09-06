cd7933b0beecdd4f48e05db052ef0f91029662ec
Aravindhan V <aravindhan.v@perfios.com>
2021-09-06 18:29:44 +0530

    ADDED: method to calculate total value

--- src/com/bridgelabz/inventorymanagement/InventoryManager.java
+++ src/com/bridgelabz/inventorymanagement/InventoryManager.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import com.bridgelabz.datastructures.LinkedList;
 import com.bridgelabz.datastructures.Node;
+import com.bridgelabz.datastructures.NodeIF;
 
 public class InventoryManager {
 	public static Scanner scanner = new Scanner(System.in);
@@ -14,7 +15,20 @@ public class InventoryManager {
 		inventoryList = new LinkedList();
 		System.out.println("Enter number of inventories");
 		int noOfInventories = Integer.parseInt(scanner.nextLine());
+		createInventories(noOfInventories);
+		calculateTotalValue();
 		
+	}
+	
+	public static void calculateTotalValue() {
+		double total = 0;
+		NodeIF<Inventory> temp = inventoryList.getHead();
+		while(temp!=null) {
+			double value = temp.getKey().calculateValue();
+			total += value;
+			temp = temp.getNext();
+		}
+		System.out.println("Total value = "+total);
 	}
 	
 	public static void createInventories(int noOfInventories) {
